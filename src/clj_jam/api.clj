71f793aa6ad39aa493eb83bd9a148045629aa7c4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-25 17:17:30 +0200

    Play a little more with the marmalade api.

--- src/clj_jam/api.clj
+++ src/clj_jam/api.clj
@@ -15,3 +15,60 @@
 ;; to retrieve the token, you are forced to use the api
 ;; (retrieve-token "username" "yourpass")
 ;; {:message "Logged in as \"username\"", :name "username", :token "some-token"}
+
+(q/api :get "/v1/packages/org-trello/0.1.5")
+
+;; {:message "Got org-trello
+;;  version 0.1.5"
+;;  :package {:downloads 109
+;;            :name "org-trello"
+;;            :owners {:ardumont "eniotna.t@gmail.com"}
+;;            :versions [{:requires [["org" [8 0 7]]
+;;                                   ["dash" [1 5 0]]
+;;                                   ["request" [0 2 0]]
+;;                                   ["cl-lib" [0 3 0]]
+;;                                   ["json" [1 2]]
+;;                                   ["elnode" [0 9 9 7 6]]
+;;                                   ["esxml" [0 3 0]]]
+;;                        :commentary nil
+;;                        :name "org-trello"
+;;                        :version [0 1 5]
+;;                        :downloads 1
+;;                        :created 1377439387
+;;                        :headers {:author "Antoine R. Dumont <eniotna.t AT gmail.com>"
+;;                                  :maintainer "Antoine R. Dumont <eniotna.t AT gmail.com>"
+;;                                  :version "0.1.5"
+;;                                  :package-requires "((org \"8.0.7\") (dash \"1.5.0\") (request \"0.2.0\") (cl-lib \"0.3.0\") (json \"1.2\") (elnode \"0.9.9.7.6\") (esxml \"0.3.0\"))"
+;;                                  :keywords "org-mode trello sync org-trello"
+;;                                  :url "https://github.com/ardumont/org-trello"}
+;;                        :type "tar"
+;;                        :description "Org minor mode to synchronize with trello"}]
+;;            :created 1373560695}}
+
+
+(q/api :get "/v1/packages/org-trello/latest")
+
+;; {:message "Got org-trello"
+;;  :package {:downloads 109
+;;            :name "org-trello"
+;;            :owners {:ardumont "eniotna.t@gmail.com"}
+;;            :versions [{:name "org-trello"
+;;                        :description "Org minor mode to synchronize with trello"
+;;                        :requires [["org" [8 0 7]]
+;;                                   ["dash" [1 5 0]]
+;;                                   ["request" [0 2 0]]
+;;                                   ["cl-lib" [0 3 0]]
+;;                                   ["json" [1 2]]
+;;                                   ["elnode" [0 9 9 7 6]]
+;;                                   ["esxml" [0 3 0]]]
+;;                        :version [0 1 5]
+;;                        :type "tar"
+;;                        :headers {:author "Antoine R. Dumont <eniotna.t AT gmail.com>"
+;;                                  :maintainer "Antoine R. Dumont <eniotna.t AT gmail.com>"
+;;                                  :version "0.1.5"
+;;                                  :package-requires "((org \"8.0.7\") (dash \"1.5.0\") (request \"0.2.0\") (cl-lib \"0.3.0\") (json \"1.2\") (elnode \"0.9.9.7.6\") (esxml \"0.3.0\"))"
+;;                                  :keywords "org-mode trello sync org-trello"
+;;                                  :url "https://github.com/ardumont/org-trello"}
+;;                        :commentary nil
+;;                        :created 1377439387}]
+;;            :created 1373560695}}
