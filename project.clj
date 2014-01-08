fd367f8acca87acf2810e38fd30e11a1a514d567
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-09 00:41:24 +0100

    Add incanter deps to begin drawing some charts

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http            "0.7.8"]
-                 [org.clojure/clojure "1.5.1"]]
+                 [org.clojure/clojure "1.5.1"]
+                 [incanter            "1.5.4"]]
   :plugins [[lein-expectations                  "0.0.7"]]
   :profiles {:dev {:dependencies [[expectations "1.4.53"]]}})
