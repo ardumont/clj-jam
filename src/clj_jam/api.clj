8a008d48fd8d9b1b3374a4d73429b6514771151a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-09 20:09:22 +0100

    Format

--- src/clj_jam/api.clj
+++ src/clj_jam/api.clj
@@ -37,6 +37,21 @@
 ;;     (package "0.0.3")
 ;;     q/execute)
 
+;; (def map-temp {:message "Got org-trello, version 0.2.5",
+;;                :package {:downloads 365,
+;;                          :name "org-trello",
+;;                          :owners {:ardumont "eniotna.t@gmail.com"},
+;;                          :versions [{:requires [["org" [8 0 7]] ["dash" [1 5 0]] ["request" [0 2 0]] ["cl-lib" [0 3 0]] ["json" [1 2]] ["elnode" [0 9 9 7 6]] ["esxml" [0 3 0]] ["s" [1 7 0]] ["kv" [0 0 17]]],
+;;                                      :commentary nil,
+;;                                      :name "org-trello",
+;;                                      :version [0 2 5],
+;;                                      :downloads 3,
+;;                                      :created 1385249629,
+;;                                      :headers {:author "Antoine R. Dumont <eniotna.t AT gmail.com>", :maintainer "Antoine R. Dumont <eniotna.t AT gmail.com>", :version "0.2.5", :package-requires "((org \"8.0.7\") (dash \"1.5.0\") (request \"0.2.0\") (cl-lib \"0.3.0\") (json \"1.2\") (elnode \"0.9.9.7.6\") (esxml \"0.3.0\") (s \"1.7.0\") (kv \"0.0.17\"))", :keywords "org-mode trello sync org-trello", :url "https://github.com/ardumont/org-trello"},
+;;                                      :type "tar",
+;;                                      :description "Org minor mode to synchronize with trello"}],
+;;                          :created 1373560695}})
+
 (def versions ^{:doc "List of current org-trello version"}
   (for [medium (range 0 3)
         minor (range 0 10)
@@ -52,6 +67,8 @@
                                         q/execute)]
                    (assoc map version (get-in info-version [:package :versions 0 :downloads] 0))))
                (sorted-map))))
+
+;; (download-by-version versions)
 
 (def actual-result {"0.0.3" 2
                     "0.0.4" 1
@@ -79,18 +96,3 @@
                     "0.2.6" 46
                     "0.2.7" 2
                     "0.2.8" 16})
-
-;; (def map-temp {:message "Got org-trello, version 0.2.5",
-;;                :package {:downloads 365,
-;;                          :name "org-trello",
-;;                          :owners {:ardumont "eniotna.t@gmail.com"},
-;;                          :versions [{:requires [["org" [8 0 7]] ["dash" [1 5 0]] ["request" [0 2 0]] ["cl-lib" [0 3 0]] ["json" [1 2]] ["elnode" [0 9 9 7 6]] ["esxml" [0 3 0]] ["s" [1 7 0]] ["kv" [0 0 17]]],
-;;                                      :commentary nil,
-;;                                      :name "org-trello",
-;;                                      :version [0 2 5],
-;;                                      :downloads 3,
-;;                                      :created 1385249629,
-;;                                      :headers {:author "Antoine R. Dumont <eniotna.t AT gmail.com>", :maintainer "Antoine R. Dumont <eniotna.t AT gmail.com>", :version "0.2.5", :package-requires "((org \"8.0.7\") (dash \"1.5.0\") (request \"0.2.0\") (cl-lib \"0.3.0\") (json \"1.2\") (elnode \"0.9.9.7.6\") (esxml \"0.3.0\") (s \"1.7.0\") (kv \"0.0.17\"))", :keywords "org-mode trello sync org-trello", :url "https://github.com/ardumont/org-trello"},
-;;                                      :type "tar",
-;;                                      :description "Org minor mode to synchronize with trello"}],
-;;                          :created 1373560695}})
