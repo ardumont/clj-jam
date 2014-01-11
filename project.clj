38dcf06b8532b0b47676da2573958cbc0a1ff600
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-11 13:36:07 +0100

    Add core-async deps to try and see what i can do to retrieve the marmalade data asynchronously

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http            "0.7.8"]
                  [org.clojure/clojure "1.5.1"]
-                 [incanter            "1.5.4"]]
+                 [incanter            "1.5.4"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
   :plugins [[lein-expectations                  "0.0.7"]]
   :profiles {:dev {:dependencies [[expectations "1.4.53"]]}})
