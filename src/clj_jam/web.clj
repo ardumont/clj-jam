d64cbcbc54c3db97c19b49475162dcdad1d37a85
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-12 12:59:17 +0100

    Add simple modification in hello.

--- src/clj_jam/web.clj
+++ src/clj_jam/web.clj
@@ -26,7 +26,7 @@
   (GET "/" []
        {:status 200
         :headers {"Content-Type" "text/plain"}
-        :body (pr-str ["Hello" :from 'Heroku])})
+        :body (pr-str ["clj-jam Hello" :from 'Heroku])})
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
