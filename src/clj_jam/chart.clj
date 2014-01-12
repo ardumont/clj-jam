d38dc6e1a52c4e34fb0346d336794694157a8033
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-12 17:02:57 +0100

    Disjoin the computation from the visual computation of the chart

--- src/clj_jam/chart.clj
+++ src/clj_jam/chart.clj
@@ -4,50 +4,52 @@
   (:require [clj-jam.api :as a]
             [clojure.string :as s]))
 
-(def versions ^{:doc "List of current org-trello versions."}
-  (-> "org-trello"
-      a/versions
-      a/downloads-by-version))
-
-;; simulate result from the api namespace
-(def versions-without-computations (into (sorted-map) {"003" 2
-                                                       "0031" 8
-                                                       "004" 1
-                                                       "0041" 8
-                                                       "005" 10
-                                                       "006" 1
-                                                       "0061" 1
-                                                       "007" 13
-                                                       "008" 20
-                                                       "009" 5
-                                                       "010" 16
-                                                       "011" 8
-                                                       "012" 8
-                                                       "013" 10
-                                                       "014" 2
-                                                       "015" 5
-                                                       "016" 10
-                                                       "017" 2
-                                                       "0171" 4
-                                                       "018" 25
-                                                       "019" 13
-                                                       "020" 20
-                                                       "021" 0
-                                                       "0211" 1
-                                                       "0212" 34
-                                                       "022" 57
-                                                       "023" 14
-                                                       "024" 1
-                                                       "025" 3
-                                                       "026" 46
-                                                       "027" 2
-                                                       "028" 19}))
-
 (defn barchart-by-versions [versions-values-map] "Given a map of versions, compute its equivalent barchart."
-  (view (bar-chart (map first versions-values-map) (map second versions-values-map)
+  (bar-chart (map first versions-values-map) (map second versions-values-map)
                    :title "Downloads per Versions"
                    :x-label "Versions"
-                   :y-label "Downloads")))
+                   :y-label "Downloads"))
 
-;; (barchart-by-versions versions-without-computations)
-;; (barchart-by-versions versions)
+(comment ;; usage
+  (def versions ^{:doc "List of current org-trello versions."}
+    (-> "org-trello"
+        a/versions
+        a/downloads-by-version))
+
+  ;; simulate result from the api namespace
+  (def versions-without-computations (into (sorted-map) {"003" 2
+                                                         "0031" 8
+                                                         "004" 1
+                                                         "0041" 8
+                                                         "005" 10
+                                                         "006" 1
+                                                         "0061" 1
+                                                         "007" 13
+                                                         "008" 20
+                                                         "009" 5
+                                                         "010" 16
+                                                         "011" 8
+                                                         "012" 8
+                                                         "013" 10
+                                                         "014" 2
+                                                         "015" 5
+                                                         "016" 10
+                                                         "017" 2
+                                                         "0171" 4
+                                                         "018" 25
+                                                         "019" 13
+                                                         "020" 20
+                                                         "021" 0
+                                                         "0211" 1
+                                                         "0212" 34
+                                                         "022" 57
+                                                         "023" 14
+                                                         "024" 1
+                                                         "025" 3
+                                                         "026" 46
+                                                         "027" 2
+                                                         "028" 19}))
+
+
+  (-> versions-without-computations barchart-by-versions view)
+  (-> versions barchart-by-versions view))
