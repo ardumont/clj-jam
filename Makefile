45b1a914944ce68417d04b1625c79aeee57735ea
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-12 12:47:29 +0100

    Add a makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+run-dev:
+	lein run -m clj-jam.web 5000
--- src/clj_jam/web.clj
+++ src/clj_jam/web.clj
@@ -1,4 +1,4 @@
-(ns helloworld.web
+(ns clj-jam.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
