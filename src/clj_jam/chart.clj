847a1e7eba5d28b7d5303e21cd306640986c872a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-12 12:13:10 +0100

    Add the missing versions.

--- src/clj_jam/chart.clj
+++ src/clj_jam/chart.clj
@@ -11,9 +11,12 @@
 
 ;; simulate result from the api namespace
 (def versions-without-computations (into (sorted-map) {"003" 2
+                                                       "0031" 8
                                                        "004" 1
+                                                       "0041" 8
                                                        "005" 10
                                                        "006" 1
+                                                       "0061" 1
                                                        "007" 13
                                                        "008" 20
                                                        "009" 5
@@ -25,17 +28,20 @@
                                                        "015" 5
                                                        "016" 10
                                                        "017" 2
+                                                       "0171" 4
                                                        "018" 25
                                                        "019" 13
                                                        "020" 20
                                                        "021" 0
+                                                       "0211" 1
+                                                       "0212" 34
                                                        "022" 57
                                                        "023" 14
                                                        "024" 1
                                                        "025" 3
                                                        "026" 46
                                                        "027" 2
-                                                       "028" 16}))
+                                                       "028" 19}))
 
 (defn barchart-by-versions [versions-values-map] "Given a map of versions, compute its equivalent barchart."
   (view (bar-chart (map first versions-values-map) (map second versions-values-map)
